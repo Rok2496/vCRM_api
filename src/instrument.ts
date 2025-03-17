d644b99f52da171fc76d452c14cd0b6392f1dd44
rok2496 <mdrokon@hotmail.com>
2025-03-17 14:09:22 +0600

    update

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,11 +1,14 @@
 const Sentry = require("@sentry/nestjs");
-const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+// Comment out the profiling integration import
+// const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 
 Sentry.init({
     dsn: "https://7ce971f5064c51da9ff944ca0fd3910e@o4508913391370240.ingest.de.sentry.io/4508913394974800",
     integrations: [
-        nodeProfilingIntegration(),
+        // Disable profiling integration
+        // nodeProfilingIntegration(),
     ],
     tracesSampleRate: 1.0,
-    profilesSampleRate: 1.0,
+    // Disable profiles sample rate
+    // profilesSampleRate: 1.0,
 });
